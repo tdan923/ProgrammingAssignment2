9d2744e1740bb6705a6cc163c7c7f03f5fcc4be8
Tdan923 <tedan923@gmail.com>
2014-10-26 16:17:46 -0700

    cachematrix.R
    Committing changes to the makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions are:
+## (1) makeCacheMatrix
+## (2) cacheSolve
 
-## Write a short comment describing this function
+## makeCacheMatrix function takes matrix input and stores the matrix in the matrix cache,
+## sets and gets the mean value of the matrix and lists as output.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	setMat <- function(newMat) {
+		x <<- newMat
+		m <<- NULL
+	}
+	getMat <- function() {
+		x
+	}
+	setMean <- function(mean) {
+		m <<- mean
+	}
+	getMean <- function() {
+		m
+	}
+	list(setMat = setMat, getMat = getMat, setMean = setMean, getMean = getMean)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	invMat <- x$getInv()
+	mat <- x$get()
+	invMat <- solve(mat)
+	x$setInv(invMat)
+	invMat
 }
